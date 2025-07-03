c1df42c1b591e1dccf533c49c6498904f8bad8ab
Asrat <asratextras77@gmail.com>
2025-07-03 17:20:08 +0300

    feat: Add has_many :comparison_stats to Creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,6 +2,7 @@ class Creator < ApplicationRecord
   has_many :telegram_posts
   has_many :github_activities
   has_many :leaderboards
+  has_many :comparison_stats
 
   validates :github_username, :telegram_channel, presence: true
   validates :github_username, :telegram_channel, uniqueness: true
