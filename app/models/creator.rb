0909732e7e21b51b102ea227d33866921c98b252
Asrat <asratextras77@gmail.com>
2025-07-03 17:18:39 +0300

    feat: Add has_many :leaderboards to Creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,4 +1,8 @@
 class Creator < ApplicationRecord
+  has_many :telegram_posts
+  has_many :github_activities
+  has_many :leaderboards
+
   validates :github_username, :telegram_channel, presence: true
   validates :github_username, :telegram_channel, uniqueness: true
 end
