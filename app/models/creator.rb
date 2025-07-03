e4dde0d89671b8f5ee9133c2443949f4c3987348
Asrat <asratextras77@gmail.com>
2025-07-03 17:02:20 +0300

    feat: Add has_many :github_activities to Creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,5 +1,6 @@
 class Creator < ApplicationRecord
   has_many :telegram_posts
+  has_many :github_activities
 
   validates :github_username, :telegram_channel, presence: true
   validates :github_username, :telegram_channel, uniqueness: true
