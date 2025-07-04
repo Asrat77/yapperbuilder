a03e80b9a8832b2a3500c938bde59b6bcc53297a
tsiige77 <tsigeweyni96@gmail.com>
2025-07-04 04:40:18 +0300

    chore: add yapperbuilder host to application config

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Yapperbuilder
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.hosts << "yapperbuilder-849543905413.europe-west1.run.app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
